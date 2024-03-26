a72a0951a282f22d8afded7b8bd17315856603d7
Nathan HENRY <kodioko97@gmail.com>
2024-03-26 08:55:35 +0100

    [config] feat: telescope update
    Search through git ignored files as well as hidden files

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader><leader>", "<cmd>Telescope find_files no_ignore=true find_command=rg,--hidden,--files<cr>" },
+    },
+    opts = {
+      defaults = {
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        no_ignore = true,
+        prompt_prefix = "🔍 ",
+        file_ignore_patterns = {
+          "node_modules",
+        },
+      },
+    },
+  },
+}
